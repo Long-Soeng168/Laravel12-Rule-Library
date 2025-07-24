34fe465b321f3050592eee1a751fd4b4c1d64ac8
KimKim168 <kimsoreya093@gmail.com>
2025-07-24 15:47:34 +0700

    dd

--- resources/js/pages/rule-library/components/my-language.tsx
+++ resources/js/pages/rule-library/components/my-language.tsx
@@ -27,7 +27,7 @@ export function MyLanguage() {
             variant="outline"
             size="icon"
             onClick={() => setValue(lang.value)}
-            className={`border-2 transition-all ${
+            className={`border-2 rounded-md transition-all ${
               value === lang.value
                 ? 'border-primary-two'
                 : 'border-transparent '
